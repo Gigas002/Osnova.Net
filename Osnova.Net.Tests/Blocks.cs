e07b633c5943c81960d3c4d67b4362116b6739c0
Gigas002 <gigas002@yandex.ru>
2021-05-08 00:16:49 +0300

    Add unit test project

---
+++ Osnova.Net.Tests/Blocks.cs
@@ -0,0 +1,37 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Osnova.Net.Responses;
+
+namespace Osnova.Net.Tests
+{
+    public class Blocks
+    {
+        public HttpClient Client { get; } = new HttpClient();
+
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public async Task GetUser()
+        {
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                WebsiteKind kind = WebsiteKind.Dtf;
+
+                var user = await User.GetUser(Client, kind, 260955);
+            });
+        }
+
+        [Test]
+        public void GetEntry()
+        {
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                WebsiteKind kind = WebsiteKind.Dtf;
+
+                var entry = await Entry.GetEntryById(Client, kind, 725050);
+            });
+        }
+    }
+}
