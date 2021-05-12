7894876abdf68a5ecc621d54b3c3cf87bcd08154
Gigas002 <gigas002@yandex.ru>
2021-05-12 22:23:33 +0300

    Implement getSearch

---
+++ Osnova.Net.Tests/SearchTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Osnova.Net.Tests
+{
+    public class SearchTests
+    {
+        [SetUp]
+        public void Setup() => Constants.CreateClient();
+
+        [Test]
+        public async Task GetSearch()
+        {
+            var entries = await Search.GetSearchAsync(Constants.Client, Constants.Kind, "yurucamp");
+        }
+    }
+}
---
+++ Osnova.Net/Search.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Osnova.Net.Enums;
+
+namespace Osnova.Net
+{
+    public enum OrderBy
+    {
+        Relevant,
+        Date
+    }
+
+    public class Search
+    {
+        #region GetSearch
+
+        public static Uri GetSearchUri(WebsiteKind websiteKind, string query, OrderBy orderBy = OrderBy.Relevant, int page = -1,
+                                       double apiVersion = Core.ApiVersion)
+        {
+            var baseUri = Core.GetBaseUri(websiteKind, apiVersion);
+
+            UriBuilder builder = new($"{baseUri}/search");
+
+            string queryString = $"query={query}";
+            string orderByString = $"order_by={orderBy.ToString().ToLowerInvariant()}";
+            string pageString = page > 0 ? $"page={page}" : null;
+
+            Core.BuildUri(ref builder, queryString, orderByString, pageString);
+
+            return builder.Uri;
+        }
+
+        public static ValueTask<HttpResponseMessage> GetSearchResponseAsync(HttpClient client, WebsiteKind websiteKind, string query,
+                                                                            OrderBy orderBy = OrderBy.Relevant, int page = -1, double apiVersion = Core.ApiVersion)
+        {
+            return Core.GetResponseFromApiAsync(client, GetSearchUri(websiteKind, query, orderBy, page, apiVersion));
+        }
+
+        public static async ValueTask<IEnumerable<Entry>> GetSearchAsync(HttpClient client, WebsiteKind websiteKind,
+                                                                         string query, OrderBy orderBy = OrderBy.Relevant, int page = -1, double apiVersion = Core.ApiVersion)
+        {
+            var response = await GetSearchResponseAsync(client, websiteKind, query, orderBy, page, apiVersion).ConfigureAwait(false);
+
+            return await Core.DeserializeOsnovaResponseAsync<IEnumerable<Entry>>(response).ConfigureAwait(false);
+        }
+
+        #endregion
+    }
+}
