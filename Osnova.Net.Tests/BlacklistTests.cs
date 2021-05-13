1f12417d125ace2d972f49e33bb792797abf5eb1
Gigas002 <gigas002@yandex.ru>
2021-05-13 23:39:15 +0300

    Implement getIgnoresHashtags

---
+++ Osnova.Net.Tests/BlacklistTests.cs
@@ -0,0 +1,21 @@
+﻿#if LOCALTESTS
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Osnova.Net.Tests
+{
+    public class BlacklistTests
+    {
+        [SetUp]
+        public void Setup() => Constants.CreateClient();
+
+        [Test]
+        public async Task GetIgnoresHashtags()
+        {
+            var tags = await Hashtag.GetIgnoresHashtagsAsync(Constants.Client, Constants.Kind).ConfigureAwait(false);
+        }
+    }
+}
+
+#endif
--- Osnova.Net/Hashtag.cs
+++ Osnova.Net/Hashtag.cs
@@ -1,9 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Osnova.Net.Enums;
 
 namespace Osnova.Net
 {
     public class Hashtag
     {
+        #region Properties
+
         [JsonPropertyName("id")]
         public long Id { get; set; }
 
@@ -12,5 +19,40 @@ namespace Osnova.Net
 
         [JsonPropertyName("content_count")]
         public int ContentCount { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        #region GET
+
+        #region GetIgnoresHashtags
+
+        public static Uri GetIgnoresHashtagsUri(WebsiteKind websiteKind, double apiVersion = Core.ApiVersion)
+        {
+            var baseUri = Core.GetBaseUri(websiteKind, apiVersion);
+
+            return new Uri($"{baseUri}/ignores/hashtags");
+        }
+
+        public static ValueTask<HttpResponseMessage> GetIgnoresHashtagsResponseAsync(HttpClient client, WebsiteKind websiteKind,
+            double apiVersion = Core.ApiVersion)
+        {
+            return Core.GetResponseFromApiAsync(client, GetIgnoresHashtagsUri(websiteKind, apiVersion));
+        }
+
+        public static async ValueTask<IEnumerable<Hashtag>> GetIgnoresHashtagsAsync(HttpClient client, WebsiteKind websiteKind,
+            double apiVersion = Core.ApiVersion)
+        {
+            using var response = await GetIgnoresHashtagsResponseAsync(client, websiteKind, apiVersion).ConfigureAwait(false);
+
+            return await Core.DeserializeOsnovaResponseAsync<IEnumerable<Hashtag>>(response).ConfigureAwait(false);
+        }
+
+        #endregion
+
+        #endregion
+
+        #endregion
     }
 }
