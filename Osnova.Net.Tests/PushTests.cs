8ce563aecb5a3bbf6e95ce9b55a8cf3b494798e2
Gigas002 <gigas002@yandex.ru>
2021-05-13 18:18:58 +0300

    Implement getUserPushTopic

---
+++ Osnova.Net.Tests/PushTests.cs
@@ -0,0 +1,21 @@
+﻿#if LOCALTESTS
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Osnova.Net.Tests
+{
+    public class PushTests
+    {
+        [SetUp]
+        public void Setup() => Constants.CreateClient();
+
+        [Test]
+        public async Task GetUserPushTopic()
+        {
+            var topic = await Push.GetUserPushTopicAsync(Constants.Client, Constants.Kind).ConfigureAwait(false);
+        }
+    }
+}
+
+#endif
--- Osnova.Net/Push.cs
+++ Osnova.Net/Push.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Osnova.Net.Enums;
 
 namespace Osnova.Net
 {
     public class Push
     {
+        #region GetUserPushTopic
+
+        public static Uri GetUserPushTopicUri(WebsiteKind websiteKind, double apiVersion = Core.ApiVersion)
+        {
+            var baseUri = Core.GetBaseUri(websiteKind, apiVersion);
+
+            return new Uri($"{baseUri}/user/push/topic");
+        }
+
+        public static ValueTask<HttpResponseMessage> GetUserPushTopicResponseAsync(HttpClient client, WebsiteKind websiteKind,
+            double apiVersion = Core.ApiVersion)
+        {
+            return Core.GetResponseFromApiAsync(client, GetUserPushTopicUri(websiteKind, apiVersion));
+        }
+
+        public static async ValueTask<string> GetUserPushTopicAsync(HttpClient client, WebsiteKind websiteKind, double apiVersion = Core.ApiVersion)
+        {
+            using var response = await GetUserPushTopicResponseAsync(client, websiteKind, apiVersion).ConfigureAwait(false);
+
+            return await Core.DeserializeOsnovaResponseAsync<string>(response).ConfigureAwait(false);
+        }
+
+        #endregion
     }
 }
